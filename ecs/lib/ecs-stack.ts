89b5dabb119b76a540403e6df0f464d36b75eb29
Nuraly Zhanbyrbayev <nuraly.zhanbyrbayev@gmail.com>
2022-06-22 21:25:34 +0600

    change ecs folder content.

--- ecs/lib/ecs-stack.ts
+++ ecs/lib/ecs-stack.ts
@@ -2,21 +2,26 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecsp from 'aws-cdk-lib/aws-ecs-patterns';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EcsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'EcsQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
     new ecsp.ApplicationLoadBalancedFargateService(this, 'AwsHustle', {
       taskImageOptions: {
         image: ecs.ContainerImage.fromRegistry(process.env.MAIN_IMAGE_LINK ?? ""),
-        
+
       },
-      
+
       publicLoadBalancer: true,
-      
+
     });
-    
   }
 }
